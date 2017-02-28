dff2496da4b1f56000b8950fafae1cfeb656abf4
Oscar <desarrollo@codigoseguro.com>
2017-02-28 12:49:18 +0100

    Chequear imagenes y empezando con las llamadas al WebService para sustituir las de MySQL

--- PhishingFisher/PhishingFisher.cs
+++ PhishingFisher/PhishingFisher.cs
@@ -3,16 +3,22 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using Freezer.Core;
+using System.Drawing.Imaging;
 
 namespace PhishingFisher
 {
     public partial class PhishingFisher : Form
     {
+        private string MyConnection2 = "datasource=codigoseguro.com;port=3306;username=desarrollo;password=eLuv326!;database=phishingFisher";
         public PhishingFisher()
         {
             InitializeComponent();
@@ -20,7 +26,8 @@ namespace PhishingFisher
 
         private void btnFIsherman_Click(object sender, EventArgs e)
         {
-            if (urlValidated(tbURL.Text)) {
+            if (urlValidated(tbURL.Text))
+            {
                 readURL(tbURL.Text);
             }
         }
@@ -28,26 +35,33 @@ namespace PhishingFisher
         /*
          * Leo el codigo HTML de la web para analizar, de momento es para hacer pruebas con URL's fijas
          */
-         
-        private void readURL(string URL){
+
+        private void readURL(string URL)
+        {
             CrawlerBait cB = new CrawlerBait();
             cB.analyzeURL(URL);
-            rtbTests.Text = cB.readWebDate(URL);
-         
-               
+
+            string html_body = cB.readWebDate(URL);
+            cB.readFiles();
+            int cant = cB.similarity;
+            insertAllSites(URL, 3, "");
+            List<string> IDs = selectAllSites(URL);
+            insertVIPS(IDs[0], URL, html_body, DateTime.Now.ToString("yyyy-MM-dd H:mm:ss"), cB.SSL_KEY, cB.SSL_EXPIRATION_DATE.ToString("yyyy-MM-dd H:mm:ss"));
+
 
         }
         /*
          * Valido que sea una URL valida
-         */ 
-        private bool urlValidated(string URL){
-            string[] arrExtensions = new string[] { ".es", ".com","http://","https://"};
+         */
+        private bool urlValidated(string URL)
+        {
+            string[] arrExtensions = new string[] { ".es", ".com", "http://", "https://" };
             foreach (var oneExten in arrExtensions)
             {
                 if (URL.Contains(oneExten))
                 {
                     return true;
-                } 
+                }
             }
             return false;
 
@@ -57,6 +71,172 @@ namespace PhishingFisher
          * 
          * 
          */
+      
+        private void btnCompareImg_Click(object sender, EventArgs e)
+        {
+            //var screenshotJob = ScreenshotJobBuilder.Create(tbURL.Text)
+            //.SetBrowserSize(600, 300)
+            //.SetCaptureZone(CaptureZone.VisibleScreen)
+            //.SetTrigger(new WindowLoadTrigger());
 
+            //System.Drawing.Image screenshot = screenshotJob.Freeze();
+            //screenshot.Save("c:/imgs/Phishing.png", ImageFormat.Png);
+            CrawlerBait cB = new CrawlerBait();
+            cB.similarityImg("c:/imgs/Paypal.png", "c:/imgs/Phishing.png");
+        }
+
+
+        /********************************************************************************
+         * Insercción de datos esto irá en un WebService, de momento lo dejaremos así para tener datos reales
+         *********************************************************************************/
+        public void insertAllSites(string webURL,int Danger,string Cloned)
+        {
+      //  string MyConnection2 = "datasource=codigoseguro.com;port=3306;username=desarrollo;password=eLuv326!;database=phishingFisher";
+
+            string sql = " insert into all_sites(URL,Danger,Cloned) values('"+webURL+"',"+Danger+",'"+Cloned+"')";
+            // string sql = " insert into vip_sites("
+            MySqlConnection cnx = new MySqlConnection(MyConnection2);
+            MySqlConnection conn = new MySqlConnection(MyConnection2);
+            conn.Open();
+            MySqlCommand comm = conn.CreateCommand();
+            comm.CommandText = sql;
+            comm.ExecuteNonQuery();
+   
+            //System.Data.SqlClient.SqlDataReader rst = cmd.ExecuteReader(System.Data.CommandBehavior.SingleResult);
+          
+        }
+        public void insertVIPS(string id,string webURL,string HTML_BODY,string LAST_HTML_CHECK,string SSL_KEY,string SSL_EXPIRATION_DATE)
+        {
+          //string MyConnection2 = "datasource=codigoseguro.com;port=3306;username=desarrollo;password=eLuv326!;database=phishingFisher";
+
+            string sql = " insert into vip_sites(ID,URL,HTML_BODY,LAST_HTML_CHECK,SSL_KEY,SSL_EXPIRATION_DATE) values("+id+",'" + webURL + "'," + "'<>'" + ",'" + LAST_HTML_CHECK + "','"+SSL_KEY+ "','"+SSL_EXPIRATION_DATE+ "')";
+            // string sql = " insert into vip_sites("
+                MySqlConnection conn = new MySqlConnection(MyConnection2);
+            conn.Open();
+            MySqlCommand comm = conn.CreateCommand();
+            comm.CommandText = sql;
+            comm.ExecuteNonQuery();
+
+            //System.Data.SqlClient.SqlDataReader rst = cmd.ExecuteReader(System.Data.CommandBehavior.SingleResult);
+
+        }
+
+        public List<string> selectAllSites(string webURL)
+        {
+         //   string MyConnection2 = "datasource=codigoseguro.com;port=3306;username=desarrollo;password=eLuv326!;database=phishingFisher";
+            // string sql = "Select * From all_sites Where URL like '%"+webURL+"'%";
+            string sql = "Select * from all_sites where URL like '%" + webURL + "%'";
+            List<string> datos = new List<string>();
+            MySqlConnection conn = new MySqlConnection(MyConnection2);
+            conn.Open();
+            MySqlCommand comm = conn.CreateCommand();
+            comm.CommandText = sql;
+           
+     
+
+	
+	        MySqlDataReader myreader = comm.ExecuteReader();
+	 
+
+	        if (myreader.Read())
+	        {
+		       
+		        datos.Add(myreader["ID"].ToString());
+
+	        }
+
+            return datos;
+        }
+       
+        public List<CrawlerBait> selectAllNewSites()
+        {
+        //    string MyConnection2 = "datasource=codigoseguro.com;port=3306;username=desarrollo;password=eLuv326!;database=phishingFisher";
+            // string sql = "Select * From all_sites Where URL like '%"+webURL+"'%";
+            string sql = "Select * from all_sites where Danger = 3";
+            List<CrawlerBait> Baits = new List<CrawlerBait>();
+            MySqlConnection conn = new MySqlConnection(MyConnection2);
+            conn.Open();
+            MySqlCommand comm = conn.CreateCommand();
+            comm.CommandText = sql;
+
+
+
+
+            MySqlDataReader myreader = comm.ExecuteReader();
+
+
+            while (myreader.Read())
+            {
+                CrawlerBait cb = new CrawlerBait(); ;
+                cb.WebURL = myreader["URL"].ToString();
+                cb.regID = Convert.ToInt32(myreader["ID"].ToString());
+                Baits.Add(cb);
+
+            }
+
+            return Baits;
+        }
+
+        public void updateAllSites(int similarity, int Danger, string Cloned,int ID)
+        {
+            // string m_strConnectionString = "Persist Security Info=False;Initial Catalog=phishingFisher;Data Source=codigoseguro.com;User ID=desarrollo;Password=eLuv326!";
+            //     string MyConnection2 = "datasource=codigoseguro.com;port=3306;username=desarrollo;password=eLuv326!;database=phishingFisher";
+
+            string sql = "update all_sites set similarity =" + similarity;
+                sql = sql + " , Danger =" + Danger + " , Cloned='" + Cloned;
+            sql = sql + " where Id =" + ID;
+            // string sql = " insert into vip_sites("
+            MySqlConnection cnx = new MySqlConnection(MyConnection2);
+            MySqlConnection conn = new MySqlConnection(MyConnection2);
+            conn.Open();
+            MySqlCommand comm = conn.CreateCommand();
+            comm.CommandText = sql;
+            comm.ExecuteNonQuery();
+
+            //System.Data.SqlClient.SqlDataReader rst = cmd.ExecuteReader(System.Data.CommandBehavior.SingleResult);
+
+        }
+        /*
+         * Esta parte irá en un Servicio de Windows, de momento se correra en este formulario
+         *
+         */
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            serviceProcess();
+
+
+        }
+        public void serviceProcess()
+        {
+          //  test();
+            List<CrawlerBait> Baits = selectAllNewSites();
+            foreach (var bait in Baits)
+            {
+                // Esto será un metodo private pero de momento lo devuelvo como atributo
+                //Es una chapuza pero igual me hace falta de momento opara hacer poruebas.
+                bait.readWebDate(bait.WebURL);
+               // bait.analyzeURL(bait.WebURL);
+                bait.readFiles();
+                if(bait.similarity > 60){
+                  //  updateAllSites(bait.similarity, 2, bait.cloned,bait.regID);
+
+                }
+            }
+        }
+
+        
+        public void test()
+        {
+           
+            es.codigoseguro.WebService1 wsp = new es.codigoseguro.WebService1();
+            es.codigoseguro.AuthHeader aoth = new es.codigoseguro.AuthHeader();
+          //  wsp.selectAllNewSites();
+            aoth.Username = "hack232";
+            aoth.Password = "hack232";
+            wsp.AuthHeaderValue = aoth;
+            wsp.selectAllNewSites();
+        }
+      
     }
-}
+    }
+
--- PhishingFisher/ThreadsCrawler/CrawlerBait.cs
+++ PhishingFisher/ThreadsCrawler/CrawlerBait.cs
@@ -11,29 +11,63 @@ using System.IO;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Text.RegularExpressions;
+using System.Security.Cryptography.X509Certificates;
+using Freezer.Core;
 
 namespace PhishingFisher.ThreadsCrawler
 {
-    class CrawlerBait
+   public class CrawlerBait
     {
 
         public string WebURL; // Direccón URL web 
         public string wasAGoodDay; // 0/1 
         public string XMLData; // Almacenamos codigo HTML
-        public string msgResult; //Resultado
-        public string regID; // ID del registro
-
+        public string cloned; //Resultado
+        public int regID; // ID del registro
+        public int similarity;
+        public string SSL_KEY;
+        public DateTime SSL_EXPIRATION_DATE;
 
         public string readWebDate(string wURL)
         {
             string sourceCode = "";
           //  try
            // {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(wURL);
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(WebURL);
                 HttpWebResponse response = (HttpWebResponse)request.GetResponse();
                 StreamReader sr = new StreamReader(response.GetResponseStream());
                 sourceCode = sr.ReadToEnd();
-                sr.Close();
+            try
+            {
+
+          /*
+           * Genero el screenshot con la libreria Freezer
+           * Para despues comparar
+           */
+            var screenshotJob = ScreenshotJobBuilder.Create(WebURL)
+              .SetBrowserSize(800, 600)
+              .SetCaptureZone(CaptureZone.FullPage)
+              .SetTrigger(new WindowLoadTrigger());
+
+            System.Drawing.Image screenshot = screenshotJob.Freeze();
+            screenshot.Save("c:/imgWebPhishing/prueba.png", ImageFormat.Png);
+            /*
+             * Guardo los datos de certificado si los tiene
+             */
+            X509Certificate cert = request.ServicePoint.Certificate;
+            X509Certificate2 cert2 = new X509Certificate2(cert);
+            string cn = cert2.GetIssuerName();
+            SSL_EXPIRATION_DATE = Convert.ToDateTime(cert2.GetExpirationDateString());
+            SSL_KEY = cert2.GetPublicKeyString();
+            }
+            catch (Exception)
+            {
+
+              
+            }
+
+
+            sr.Close();
                 response.Close();
                 return sourceCode;
             //}
@@ -41,27 +75,9 @@ namespace PhishingFisher.ThreadsCrawler
            // { sourceCode = "ERROR"; }
           //  return sourceCode;
         }
-        // string linea, contenido = "";
-        // HttpClient cliente = new HttpClient();
-        // linea = await respuesta.Content.ReadAsStringAsync();
-
-        //  return linea;
-
-
-        // HttpWebRequest wRequest = WebRequest.Create(wURL) as HttpWebRequest;
-        // // para que lo devuelva como si accediéramos con un Smartphone
-        //// wRequest.UserAgent = "Mozilla/4.0 (compatible; MSIE 4.01; Windows CE; Smartphone; 176x220)";
-
-        // // Obtener la respuesta y abrir el stream de la respuesta recibida.
-        // StreamReader reader = new StreamReader(wRequest.GetResponse().GetResponseStream());
-
-        // string res = reader.ReadToEnd();
-        // reader.Close();
-        // return res;
-        // Mostrarlo.
-        // Console.WriteLine(res);
-
-        // Cerrar el stream abierto.
+        /* 
+         * Saco las imagenes colgadas en la web
+         */
         public void analyzeURL(string webURL){
             String htmlWEB = readWebDate(webURL);
             int i = 0;
@@ -77,14 +93,36 @@ namespace PhishingFisher.ThreadsCrawler
                         {
                             i++;
                             string ext ="." + oneSpLink.Split('.')[oneSpLink.Split('.').Length-1];
+                            if (oneSpLink.Contains("http://") || oneSpLink.Contains("https://"))
+                            {
+                                SaveImage(oneSpLink, "c:/imgs/" + i + ext, returnImgFormat(ext));
 
-                            SaveImage(oneSpLink, "c:/imgs/"+i + ext, returnImgFormat(ext));
+                            }
+                            else
+                            {
+                                string[] arrRootURL = webURL.Split('/');
+                                string rootURL = "";
+                                int auxI = 0;
+                                foreach (var splitRootURL in arrRootURL)
+                                {
+                                    if (auxI == arrRootURL.Length-1) { break; }
+
+                                    rootURL = rootURL +splitRootURL +"/";
+                                    auxI++;
+                                    
+                                }
+                                SaveImage(rootURL+oneSpLink, "c:/imgWebPhishing/" + i + ext, returnImgFormat(ext));
+
+                            }
+
+
                             //Console.Write();
                         }
                     }
                      
                     }
                 }
+
             }
         //  String links = htmlWEB.Split('=')[1];
 
@@ -118,21 +156,112 @@ namespace PhishingFisher.ThreadsCrawler
         }
         return false;
     }
-      
+      /*
+       * Guardo las imagenes que previamente he analizado del codigo HTML
+       */
         public void SaveImage(string imageURL,string filename, ImageFormat format)
         {
+            try
+            {
 
-            WebClient client = new WebClient();
-            Stream stream = client.OpenRead(imageURL);
-            Bitmap bitmap; bitmap = new Bitmap(stream);
+                WebClient client = new WebClient();
+                Stream stream = client.OpenRead(imageURL);
+                Bitmap bitmap; bitmap = new Bitmap(stream);
 
-            if (bitmap != null)
-                bitmap.Save(filename, format);
+                if (bitmap != null)
+                    bitmap.Save(filename, format);
 
-            stream.Flush();
-            stream.Close();
-            client.Dispose();
+                stream.Flush();
+                stream.Close();
+                client.Dispose();
+            }
+            catch (Exception)
+            {
+
+            }
+         
         }
+        /*
+         * Compruebo si los pixels de las imagenes se parecen
+         */
+        public double similarityImg(string fname1, string fname2)
+        {
+            // progressBar1.Visible = true;
+            string img1_ref, img2_ref;
+            Bitmap img1 = new Bitmap(fname1);
+            Bitmap img2 = new Bitmap(fname2);
+            double countPixDif = 0;
+            double countPixSim = 0;
+            bool flag = true;
+            // progressBar1.Maximum = img1.Width;
+            if (img1.Width == img2.Width && img1.Height == img2.Height)
+            {
+                for (int i = 0; i < img1.Width; i++)
+                {
+                    for (int j = 0; j < img1.Height; j++)
+                    {
+                        img1_ref = img1.GetPixel(i, j).ToString();
+                        img2_ref = img2.GetPixel(i, j).ToString();
+                        if (img1_ref != img2_ref)
+                        {
+                            countPixDif++;
+                            flag = false;
+                            break;
+                        }
+                        countPixSim++;
+                    }
+                    // progressBar1.Value++;
+                }
+                //  if (flag == false)
+                //   MessageBox.Show("Sorry, Images are not same , " + countPixSim + " wrong pixels found");
+                //    else
+                //    MessageBox.Show(" Images are same , " + countPixSim+ " same pixels found and " + countPixDif + " wrong pixels found");
+            }
+            else
+                return 0;// no se pueden comparar
+
+         //   this.Dispose();
+            Double percentage;
+
+            percentage = (countPixSim / (countPixSim + countPixDif)) * 100;
+            return percentage;
+        }
+        public void readFiles()
+        {
+            foreach (var dir in Directory.GetDirectories("c:/imgs"))
+            {
+                foreach (var file in Directory.GetFiles(dir))
+                {
+                    foreach (var item in Directory.GetFiles("c:/imgWebPhishing"))
+                    {
+                        int cant = Convert.ToInt32(similarityImg(item, file));
+                        if (cant > 60)
+                        {
+                            similarity = cant;
+                            cloned = dir.ToString();
+                        }
+                    }
+                }
+            }
+        }
+        public void takeScreenshot(System.Windows.Forms.WebBrowser WBScreenShot)
+        {
+            //try
+            //{
+
+            //    Bitmap docImage = new Bitmap(600, 800);
+            //    WBScreenShot.DrawToBitmap(docImage, new Rectangle(WBScreenShot.Location.X, WBScreenShot.Location.Y, WBScreenShot.Width, WBScreenShot.Height));
+            //   // this.picDoc.Image = this.picDoc.getSizedImage(docImage);
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //}
+
+
+        }
+
         internal static async Task<string> obtainHTML(HttpResponseMessage respuesta, Uri URL)
         {
             string linea, contenido = "";
